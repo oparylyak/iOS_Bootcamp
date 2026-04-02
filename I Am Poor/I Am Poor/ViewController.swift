ab74d68021c51864b1edf178e0e340cc4d83231d
Oleh Paryliak <olehparylyak@gmail.com>
2026-04-02 21:15:04 +0300

    feat: git init for iOS Bootcamp | 2h

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Layer on 02.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
