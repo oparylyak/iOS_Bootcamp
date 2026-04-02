09615bc1cd6b183b8c7cda94803892e10a148ec6
Oleh Paryliak <olehparylyak@gmail.com>
2026-04-02 22:46:58 +0300

    fix: add I Am Rich app | 10m

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Layer on 10.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
