3731ad0925772de27b0037227012e19ad721bf98
Oleh Paryliak <olehparylyak@gmail.com>
2026-04-06 22:41:19 +0300

    feature: iOSBootcamp up to lesson 49 | 3h 15m

--- Dicee-iOS26/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS26/Dicee-iOS13/ViewController.swift
@@ -9,27 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     // Interface builder outlet - IBOutlet
     // Main.storyboard - interface builder
     @IBOutlet weak var diceImageViewLeft: UIImageView!
     @IBOutlet weak var diceImageViewRight: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageViewLeft.image = UIImage(named: getRandomDiceName())
-        diceImageViewRight.image = UIImage(named: getRandomDiceName())
-    }
-    
-    private func getRandomDiceName() -> String {
-        let dices = ["One", "Two", "Three", "Four", "Five", "Six"]
-        let randomDiceNumber = dices.randomElement() ?? "One"
-        let randomDice = "Dice\(randomDiceNumber)"
         
-        return randomDice
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageViewLeft.image = diceArray.randomElement()
+        diceImageViewRight.image = diceArray.randomElement()
     }
 }
+
+//    @IBAction func rollButtonPressed(_ sender: UIButton) {
+//        My realization for current lessons with func below
+//        diceImageViewLeft.image = UIImage(named: getRandomDiceName())
+//        diceImageViewRight.image = UIImage(named: getRandomDiceName())
+//        My realization for current lessons with func below
+//
+//        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+//        
+//        diceImageViewLeft.image = diceArray[Int.random(in: 0...5)]
+//        diceImageViewRight.image = diceArray[Int.random(in: 0...5)]
+//    }
+//    
+//    private func getRandomDiceName() -> String {
+//        
+//        let dices = ["One", "Two", "Three", "Four", "Five", "Six"]
+//        
+//        let randomDiceNumber = dices.randomElement() ?? "One"
+//        let randomDice = "Dice\(randomDiceNumber)"
+//        
+//        return randomDice
+//    }
+
 
