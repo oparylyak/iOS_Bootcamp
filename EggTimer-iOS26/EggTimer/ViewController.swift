cf08a7759caeac76872c081fa8c3fa3d8585368a
Oleh Paryliak <olehparylyak@gmail.com>
2026-04-17 16:14:40 +0300

    feat: commit all changes | 5m

--- EggTimer-iOS26/EggTimer/ViewController.swift
+++ EggTimer-iOS26/EggTimer/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    let hardnesesArray = ["Soft", "Medium", "Hard"]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        
+        if hardness == hardnesesArray[0] {
+            print(softTime)
+        } else if hardness == hardnesesArray[1] {
+            print(mediumTime)
+        } else if hardness == hardnesesArray[2] {
+            print(hardTime)
+        }
+        
+        loveCalculator()
+    }
+    
+    func loveCalculator() {
+        let loveScore = Int.random(in: 0 ... 100)
+        
+        if loveScore > 80 {
+            print("You love each other like Kaney loves Kaney \(loveScore)")
+        } else if loveScore > 40 {
+            print ("You go together like Coke an Mentos \(loveScore)")
+        } else {
+            print ("You`ll be forever alone \(loveScore)")
+        }
+    }
 }
